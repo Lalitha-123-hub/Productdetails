6cdb7e20ff644ce8c77ff40db192aeafdd0dd6fc
dell <dell@DESKTOP-KB6FBG3>
2024-12-19 18:06:02 +0530

    Add project files.

---
+++ Productdetails/Product.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Productdetails
+{
+    internal class Product
+    {
+        int ProductId;
+        string ProductName;
+        string Quantity;
+        double Price;
+        public Product(int ProductId, string ProductName, string Quantity,double Price)
+        {
+            this.ProductId = ProductId;
+            this.ProductName = ProductName;
+            this.Quantity = Quantity;
+            this.Price = Price;
+        }
+        public void ShowProduct()
+        {
+            Console.WriteLine(ProductId + " " + ProductName + " " + Quantity + " " + Price);
+                  
+        }
+    }
+}
+
---
+++ Productdetails/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Productdetails
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Product p1= new Product(9001,"Apple","1Kg",199.00);
+            p1 .ShowProduct();
+            Product p2 = new Product(9002,"Mango","12pc",399.99);
+            p2 .ShowProduct();
+            
+        }
+    }
+}
